06ceb7299a16970f44f1b7dbead99df455f220b5
Aarthy Ravi <aarthydravi@gmail.com>
2017-07-03 14:01:24 -0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,1 +1,113 @@
+var initialLocation = [
+  {
+    title: 'Laurelwood School',
+    location: {lat: 37.3431,lng: -122.0022},
+  },
+  {
+   title: 'Kaiser Permanente Medical Center',
+   location: {lat: 37.336,lng: -121.9979},
+ },
+ {
+   title: 'Burger King',
+   location: {lat: 37.3519, lng: -121.9998},
+ }
+]
+var locationInfo = function(data){
+  this.title = (data.title);
+  this.location = (data.location);
+}
 
+var ViewModel = function(){
+  var self = this;
+
+  this.locationList = ko.observableArray([]);
+
+  initialLocation.forEach(function(locationItem){
+    self.locationList.push(new locationInfo(locationItem));
+  },this);
+
+  this.currentLocation = ko.observable(this.locationList()[0]);
+
+  this.changeLocation = function(clickLocation) {
+    // Set current location to which user clicked.
+		self.currentLocation(clickLocation);
+    var index = self.locationList().indexOf(clickLocation);
+    var position1 = initialLocation[index].location;
+    var title1 = initialLocation[index].title;
+
+    // Create a marker per location, and put into markers array.
+    var marker1 = new google.maps.Marker({
+      map: map,
+      position: position1,
+      title: title1,
+      id: index
+    });
+    populateInfoWindow(marker1, infowindow);
+  }
+
+      // Initialize Google map
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 37.3541, lng: -121.9552},
+    zoom: 13
+  });
+  // Initialize markers
+	var markers = [];
+  // Initialize infowindow
+	var infowindow = new google.maps.InfoWindow();
+
+
+  // The following group uses the location array to create an array of markers on initialize.
+  for (var i = 0; i < initialLocation.length; i++) {
+
+    // Get the position from the location array.
+    var position = initialLocation[i].location;
+    var title = initialLocation[i].title;
+
+    // Create a marker per location, and put into markers array.
+    var marker = new google.maps.Marker({
+      map: map,
+      position: position,
+      title: title,
+      animation: google.maps.Animation.DROP,
+      id: i
+    });
+
+   markers.push(marker);
+
+  // Create an onclick event to open an infowindow at each marker.
+  marker.addListener('click', function() {
+    populateInfoWindow(this, infowindow);
+  });
+}
+
+
+  // This function populates the infowindow when the marker is clicked. We'll only allow
+  // one infowindow which will open at the marker that is clicked, and populate based
+  // on that markers position.
+  function populateInfoWindow(marker, infowindow) {
+    // Check to make sure the infowindow is not already opened on this marker.
+  if (infowindow.marker != marker) {
+      infowindow.marker = marker;
+      infowindow.setContent('');
+
+      // Make sure the marker property is cleared if the infowindow is closed.
+      infowindow.addListener('closeclick',function(){
+        infowindow.setMarker = null;
+      });
+      var geocoder = new google.maps.Geocoder();
+      geocoder.geocode({'location': marker.position}, function(results, status) {
+          if (status === google.maps.GeocoderStatus.OK) {
+            formaddress = results[0].formatted_address;
+            infowindow.setContent('<div>' + marker.title + '</div><div>' + formaddress + '</div>');
+
+        }
+      });
+
+      // Open the infowindow on the correct marker
+      infowindow.open(this.map, marker);
+    }
+  }
+}
+
+// Initialize the Knockout View Model
+ko.applyBindings(new ViewModel());
