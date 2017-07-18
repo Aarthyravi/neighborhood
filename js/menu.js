5e667b2f367e407f6f2d62e251f52384d66d2ac2
Aarthy Ravi <aarthydravi@gmail.com>
2017-07-18 16:44:27 -0700

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+/*
+ * Open the drawer when the menu ison is clicked.
+ */
+var menu = document.querySelector('#menu');
+var drawer = document.querySelector('.nav');
+menu.addEventListener('click', function(e) {
+  drawer.classList.toggle('open');
+  e.stopPropagation();
+});
+$( '.nav li' ).on("click", function(){
+  $('.nav').removeClass('open');
+});
